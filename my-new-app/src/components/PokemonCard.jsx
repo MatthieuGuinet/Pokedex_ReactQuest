724238063ef2352a49ff9c8afe63ceb4268ffd66
MatthieuGuinet <matthieuguinet@gmail.com>
2023-03-28 11:16:37 +0200

    change title of page and just adding a className to the card figure

--- my-new-app/src/components/PokemonCard.jsx
+++ my-new-app/src/components/PokemonCard.jsx
@@ -1,6 +1,6 @@
 export default function PokemonCard() {
     return (
-        <figure>
+        <figure className = 'card'>
             <img
             src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
             alt="Bulbasaur"
