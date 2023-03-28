33799862f7ca501f3eb85d99bbd427234c3954f2
MatthieuGuinet <matthieuguinet@gmail.com>
2023-03-28 11:38:05 +0200

    commit for the second component of the quest

--- my-new-app/src/components/PokemonCard.jsx
+++ my-new-app/src/components/PokemonCard.jsx
@@ -1,11 +1,24 @@
 export default function PokemonCard() {
+    const pokemon = pokemonList[1]
+
     return (
         <figure className = 'card'>
             <img
-            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-            alt="Bulbasaur"
+            src={pokemon.imgSrc}
+            alt={pokemon.name}
             />
-            <figcaption>Bulbasaur</figcaption>
+            <figcaption>{pokemon.name === 'bulbasaur' ? pokemon.name : <p>???</p>}</figcaption>
         </figure>
     )
 }
+
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
