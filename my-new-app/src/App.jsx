66f2553918247b6688c6120d406d90f1ec038f0a
MatthieuGuinet <matthieuguinet@gmail.com>
2023-03-28 16:31:05 +0200

    Commit for the props.1

--- my-new-app/src/App.jsx
+++ my-new-app/src/App.jsx
@@ -1,18 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import PokemonCard from './components/PokemonCard'
 
 
-function App() {
-  const [count, setCount] = useState(0)
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
-  return (
-    <div className="Card">
-      <PokemonCard />
-    </div>
-  )
+function App() {
+  return <PokemonCard pokemon={pokemonList}/>
 }
 
 export default App
--- my-new-app/src/components/PokemonCard.jsx
+++ my-new-app/src/components/PokemonCard.jsx
@@ -1,22 +1,14 @@
-export default function PokemonCard() {
-    const pokemon = pokemonList[0]
-    const igmPokemon = pokemon.imgSrc ? (<img src={pokemon.imgSrc} alt={pokemon.name} />) : (<p>???</p>)
+function PokemonCard({pokemon}) {
+    const {name, imgSrc} = pokemon[0]
+    const igmPokemon = imgSrc ? (<img src={imgSrc} alt={name} />) : (<p>???</p>)
 
     return (
         <figure className = 'card'>
             {igmPokemon}
-            <figcaption>{pokemon.name}</figcaption>
+            <figcaption>{name}</figcaption>
         </figure>
     )
 }
 
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
+export default PokemonCard
+
